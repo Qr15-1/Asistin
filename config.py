cc4ca3b4a2d94cead096312dd525c46e70ca1117
Roger <roger@example.com>
2026-07-07 09:48:59 -0400

    Actualizacion de marcas: eliminacion de marcas inactivas y adicion de entregas mensuales para Roger

--- config.py
+++ config.py
@@ -29,23 +29,23 @@ ADMIN_IDS = [515198765, 5499547223]
 # CALENDARIOS
 # Calendario Semanal (Dia -> Responsable -> Marcas)
 CALENDARIO_SEMANAL = {
-    "Monday": { "R": ["Emiliarte",], "F": ["Agro PDK", "Pizza De Verdad"], "Roger": ["El Toque"] },
+    "Monday": { "R": ["Emiliarte",], "F": [], "Roger": ["El Toque"] },
     "Tuesday": { "R": ["Luva"], "F": ["+58 Shop", "La Zapeteria"], "Roger": ["Osersa"] },
     "Wednesday": { "R": ["Altamar"], "F": [] },
-    "Thursday": { "R": ["Dra. K Beauty", "Dermelite"], "F": ["Bungerz"] },
-    "Friday": { "R": ["La Cava"], "F": ["La Cascada"] }
+    "Thursday": { "R": ["Dra. K Beauty"], "F": ["Bungerz"] },
+    "Friday": { "R": ["La Cava"], "F": [] }
 }
 
 # Calendario Mensual (Dia del mes DD -> Marcas)
 DIAS_MENSUALES = {
-    "03": ["Dra. K Beauty", "Agro PDK"],
+    "03": ["Dra. K Beauty"],
+    "05": ["El Toque"],
     "06": ["La Zapeteria"],
     "09": ["Bungerz"],
     "10": ["Emiliarte"],
-    "12": ["La Cava", "Emiliarte", "La Cascada"],
+    "12": ["La Cava", "Emiliarte"],
     "13": ["Luva", "Altamar"],
     "15": ["Chaofan"],
 
-    "18": ["+58 Shop"],
-    "25": ["Pizza De Verdad"]
+    "18": ["+58 Shop", "Osersa"]
 }
